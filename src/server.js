4d43088798ba9848011e94ff1bf768afc96ba500
dark1922 <jvdematos004@gmail.com>
2021-10-04 00:49:43 -0300

    tsv

--- src/server.js
+++ src/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/ProjetoAngular'));
+app.use(express.static(__dirname + '/dist/ProjetoAngular'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/ProjetoAngular/'}),
+    res.sendFile(path.join,(__dirname +' /dist/ProjetoAngular/index.html' )),
 );
 
 // Start the app by listening on the default Heroku port
