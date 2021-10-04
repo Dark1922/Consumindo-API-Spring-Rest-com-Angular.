054c96457df53a828b61cb93c6a359516199f727
João Victor <jvdematos004@gmail.com>
2021-10-04 01:55:12 -0300

    Add files via upload

---
+++ src/app/app-constants (1).ts
@@ -0,0 +1,19 @@
+export class AppConstants {
+
+  //base do nosso servidor a url padrão
+  public static get baseServidor(): string {
+    return 'http://apirestspringakx.herokuapp.com/';
+  }
+
+  //url login
+  public static get baseLogin(): string {
+    return this.baseServidor + 'cursospringrestapi/login';
+  }
+
+  public static get baseUrl(): string {
+    return this.baseServidor + 'cursospringrestapi/usuario/'}
+
+    public static get getBaseUrlPath() : string {return this.baseServidor + 'cursospringrestapi/'}
+
+
+}
